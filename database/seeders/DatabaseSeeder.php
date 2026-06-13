c01522ea7c95fe018e3ca192891f296caa076c64
Pedro Martin <pedromartintorrijo@gmail.com>
2026-06-13 17:36:39 +0200

    Corregido UserFactory inicializando Faker de forma nativa2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\User::updateOrCreate(
+            ['email' => 'admin@artiemhotels.com'], // Evita duplicados si lo ejecutas más veces
+            [
+                'name' => 'Admin Artiem',
+                'password' => \Illuminate\Support\Facades\Hash::make('artiem2026'),
+                'email_verified_at' => now(),
+            ]
+        );
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
